2b2662857089b5ed7efe8a18c0146529978c8898
killthezero <verpadora@naver.com>
2021-11-29 23:35:22 +0900

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -37,7 +37,7 @@ let clickChangeFruit = false;
 // 广告链接：false 或为空字符串表示不会跳转到广告
 let adLink = 'https://t.me/kill_the_zero';
 
-// 修改网页标题：将 "合成大西瓜" 进行替换
+// 修改网页标题：将 "KillTheZero" 进行替换
 document.getElementsByTagName("title")[0].innerText = 'KillTheZero';
 
 // 开启选分弹窗：将 false 改为 true
